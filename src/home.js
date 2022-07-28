4992c9a941cf2c9e08fd4ebe1f1267348e3e9879
Raul Jimenez <rjbarco@gmail.com>
2022-07-28 13:09:42 +0200

    Added some new images. Now shared content is generated in 'index.js'. Different content will be generated in different modules

--- src/home.js
+++ src/home.js
@@ -1,33 +1,8 @@
-import Logo from './super-mario-logo.svg';
-//import Logo2 from './Super-Mario-Logo-500x313.jpg';
-
 export default function homeLoad() {
-    const content = document.getElementById('content');
-    const header = document.createElement('div');
-    const headline = document.createElement('div');
-    const hdTop = document.createElement('div');
-    const hdDown = document.createElement('div');
-    const title = document.createElement('h1');
+    const headline = document.getElementById('headline');
     const caption = document.createElement('p');
-    const myLogo = new Image();
-    myLogo.src = Logo;
 
-    header.setAttribute('id','header');
-    headline.setAttribute('id','headline');
-    hdTop.setAttribute('id','hd-top');
-    hdDown.setAttribute('id','hd-down');
     caption.setAttribute('class','caption');
-
-    hdTop.appendChild(myLogo);
-    
-    title.innerHTML = "Restaurant";
-    hdDown.appendChild(title);
-
     caption.innerHTML = "Come on and test our Mushroom Kingdom incredible pizzas. We cook all of them with half love and half magic. Once you taste one of them, you fall in love with it. Peach Princess at the recipes, Mario and Luigi Brothers at the management and Toad Co. at the tools. What are you waiting for? I'm watching your stomach roars!!! :-)";
     headline.appendChild(caption);
-
-    header.appendChild(hdTop);
-    header.appendChild(hdDown);
-    content.appendChild(header);
-    content.appendChild(headline);
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,60 @@
 import homeLoad from './home.js';
 import './style.css';
+import Logo from './super-mario-logo.svg';
 
+function fixedContentLoad() {
+    const content = document.getElementById('content');
+    const header = document.createElement('div');
+    const menu = document.createElement('div');
+    const homeOption = document.createElement('div');
+    const menuOption = document.createElement('div');
+    const contactOption = document.createElement('div');
+    const homeOptionText = document.createElement('p');
+    const menuOptionText = document.createElement('p');
+    const contactOptionText = document.createElement('p');
+    const headline = document.createElement('div');
+    const hdTop = document.createElement('div');
+    const hdDown = document.createElement('div');
+    const title = document.createElement('h1');
+    const main = document.createElement('div');
+
+    const myLogo = new Image();
+    myLogo.src = Logo;
+
+    header.setAttribute('id','header');
+    menu.setAttribute('id','menu');
+    headline.setAttribute('id','headline');
+    hdTop.setAttribute('id','hd-top');
+    hdDown.setAttribute('id','hd-down');
+    main.setAttribute('id','main');
+
+    homeOption.setAttribute('class','menu-btn');
+    menuOption.setAttribute('class','menu-btn');
+    contactOption.setAttribute('class','menu-btn');
+
+    homeOptionText.innerHTML = 'Home';
+    menuOptionText.innerHTML = 'Menu';
+    contactOptionText.innerHTML = 'Contact';
+
+    homeOption.appendChild(homeOptionText);
+    menuOption.appendChild(menuOptionText);
+    contactOption.appendChild(contactOptionText);
+    
+    menu.appendChild(homeOption);
+    menu.appendChild(menuOption);
+    menu.appendChild(contactOption);
+
+    hdTop.appendChild(myLogo);
+    
+    title.innerHTML = "Restaurant";
+    hdDown.appendChild(title);
+
+    header.appendChild(hdTop);
+    header.appendChild(hdDown);
+    content.appendChild(header);
+    content.appendChild(menu);
+    content.appendChild(headline);
+}
+
+fixedContentLoad();
 homeLoad();
